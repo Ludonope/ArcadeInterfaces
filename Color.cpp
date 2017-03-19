e810a7d1a2408a1b9adfa687672dd480db2c70dd
PierreBougon <pierre.bougon@epitech.eu>
2017-03-19 04:43:21 +0100

    New way to create colors + add some basic colors

--- Color.cpp
+++ Color.cpp
@@ -2,21 +2,16 @@
 
 namespace arcade
 {
-	Color make_color(uint32_t c)
-	{
-		Color color = { c };
+  Color::Color(uint32_t c)
+  {
+  	full = c;
+  }
 
-		return (color);
-	}
-
-	Color make_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
-	{
-		Color color;
-
-		color.rgba[0] = r;
-		color.rgba[1] = g;
-		color.rgba[2] = b;
-		color.rgba[3] = a;
-		return (color);
-	}
+  Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+  {
+  	rgba[0] = r;
+  	rgba[1] = g;
+  	rgba[2] = b;
+  	rgba[3] = a;
+  }
 }
