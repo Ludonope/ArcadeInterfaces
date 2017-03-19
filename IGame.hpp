ee94973aa3861e8d3a0937f31dbd7a98bb2df278
Ludovic Petrenko <ludovic.petrenko@epitech.eu>
2017-03-19 03:29:55 +0100

    Added process method to IGame and fixed typo

--- IGame.hpp
+++ IGame.hpp
@@ -1,4 +1,4 @@
-// v1.0.1
+// v1.0.2
 
 #ifndef IGAME_HPP_
 #define IGAME_HPP_
@@ -30,9 +30,12 @@ namespace arcade
     virtual void notifyNetwork(std::vector<NetworkPacket> const &events) = 0;
     virtual std::vector<NetworkPacket> getNetworkToSend() const = 0;
 
+    // Process
+    virtual void process() = 0;
+
     // Sound
     virtual std::vector<std::string> getSoundsToLoad() const = 0;
-    virtual std::vector<int>         getSoundSToPlay() const = 0;
+    virtual std::vector<int>         getSoundsToPlay() = 0;
 
     // Map
     virtual IMap const &getCurrentMap() const = 0;
