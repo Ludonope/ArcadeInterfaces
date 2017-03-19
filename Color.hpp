24ae77697810124ef5924d096644a1d48ee80d0c
Ludovic Petrenko <ludovic.petrenko@epitech.eu>
2017-03-19 06:04:32 +0100

    Basic color moved as static const Color member Used initializer list instead of assigning in constructor body

--- Color.hpp
+++ Color.hpp
@@ -10,25 +10,22 @@ namespace arcade
     Color();
     Color(uint32_t c);
     Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255);
-    Color(Color &c);
+    Color(Color const &c);
 
-    uint32_t full;
+	// Basic constant colors
+	static const Color Black;
+	static const Color White;
+	static const Color Red;
+	static const Color Green;
+	static const Color Blue;
+	static const Color Yellow;
+	static const Color Magenta;
+	static const Color Cyan;
+	static const Color Transparent;
+	
+	uint32_t full;
     uint8_t  rgba[4];
   };
-
-  // Define basic constant colors
-  namespace color
-  {
-      const Color Black(0, 0, 0);
-      const Color White(255, 255, 255);
-      const Color Red(255, 0, 0);
-      const Color Green(0, 255, 0);
-      const Color Blue(0, 0, 255);
-      const Color Yellow(255, 255, 0);
-      const Color Magenta(255, 0, 255);
-      const Color Cyan(0, 255, 255);
-      const Color Transparent(0, 0, 0, 0);
-  }
 }
 
 #endif // !COLOR_HPP_
