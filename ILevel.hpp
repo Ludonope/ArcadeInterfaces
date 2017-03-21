5d42df544f3172f5d0dcd98208d9e48729b7942d
PierreBougon <pierre.bougon@epitech.eu>
2017-03-21 18:14:51 +0100

    Update ILevel => add getter / setter for levelMap and levelDifficulty

--- ILevel.hpp
+++ ILevel.hpp
@@ -1,14 +1,23 @@
 #ifndef ILEVEL_HPP_
 #define ILEVEL_HPP_
 
+#include <memory>
+
 namespace arcade
 {
-  class ILevel
-  {
-  public:
-    virtual ~ILevel(){};
-    // NOT DEFINED YET
-  };
+    class ILevel
+    {
+    public:
+        // TODO
+
+        virtual                         ~ILevel(){};
+
+        virtual std::unique_ptr<IMap>   getMap() const = 0;
+        virtual unsigned int            getDifficulty() const = 0;
+
+        virtual void                    setMap(const std::unique_ptr<IMap> levelMap) = 0;
+        virtual void                    setDifficulty(unsigned int levelDifficulty) = 0;
+    };
 }
 
 #endif // !ILEVEL_HPP_
