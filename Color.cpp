9a664edfb4316d2a90ae79856f6d8bb375bd7a9d
Ludovic Petrenko <ludovic.petrenko@epitech.eu>
2017-03-24 23:49:08 +0100

    Improved color usage

--- Color.cpp
+++ Color.cpp
@@ -2,26 +2,26 @@
 
 namespace arcade
 {
-  Color::Color(uint32_t c)
-  {
-  	full = c;
-  }
+	// Basic constant colors
+	const Color Color::Black(0, 0, 0);
+	const Color Color::White(255, 255, 255);
+	const Color Color::Red(255, 0, 0);
+	const Color Color::Green(0, 255, 0);
+	const Color Color::Blue(0, 0, 255);
+	const Color Color::Yellow(255, 255, 0);
+	const Color Color::Magenta(255, 0, 255);
+	const Color Color::Cyan(0, 255, 255);
+	const Color Color::Transparent(0, 0, 0, 0);
 
-  Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
-  {
-  	rgba[0] = r;
-  	rgba[1] = g;
-  	rgba[2] = b;
-  	rgba[3] = a;
-  }
+	Color::Color(uint32_t c) :
+		full(c)
+	{}
 
-  Color::Color()
-  {
-	full = color::Black.full;
-  }
+	Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) :
+		r(r), g(g), b(b), a(a)
+	{}
 
-  Color::Color(Color &c)
-  {
-    full = c.full;
-  }
+	Color::Color(Color const &c) :
+		full(c.full)
+	{}
 }
--- Color.hpp
+++ Color.hpp
@@ -3,32 +3,72 @@
 
 #include <cstdint>
 
+#define IS_LITTLE_ENDIAN (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+
 namespace arcade
 {
-  union Color
-  {
-    Color();
-    Color(uint32_t c);
-    Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255);
-    Color(Color &c);
+	///
+	/// \union Color
+	/// \brief Union to store RGBa color
+	///
+	union Color
+	{
+		///
+		/// \fn Color(uint32_t c = Black.full)
+		/// \brief Constructor to create color from a 4 bytes integer
+		/// \param[in] c 4 bytes integer value containing a color
+		///
+		Color(uint32_t c = Black.full);
 
-    uint32_t full;
-    uint8_t  rgba[4];
-  };
+		///
+		/// \fn Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255)
+		/// \brief Constructor to create color from a 4 color component
+		/// \param[in] r Red color component value
+		/// \param[in] g Green color component value
+		/// \param[in] b Blue color component value
+		/// \param[in] a Alpha (opacity) color component value
+		///
+		Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255);
 
-  // Define basic constant colors
-  namespace color
-  {
-      const Color Black(0, 0, 0);
-      const Color White(255, 255, 255);
-      const Color Red(255, 0, 0);
-      const Color Green(0, 255, 0);
-      const Color Blue(0, 0, 255);
-      const Color Yellow(255, 255, 0);
-      const Color Magenta(255, 0, 255);
-      const Color Cyan(0, 255, 255);
-      const Color Transparent(0, 0, 0, 0);
-  }
+		///
+		/// \fn Color(Color const &c)
+		/// \brief Constructor to copy a Color
+		/// \param[in] c Color to be copied
+		///
+		Color(Color const &c);
+
+
+		static const Color Black; /// Black color
+		static const Color White; /// White color
+		static const Color Red; /// Red color
+		static const Color Green; /// Green color
+		static const Color Blue; /// Blue color
+		static const Color Yellow; /// Yellow color
+		static const Color Magenta; /// Magenta color
+		static const Color Cyan; /// Cyan color
+		static const Color Transparent; /// Transparent color
+
+		uint32_t full; /// 4 bytes integer value of the color
+		uint8_t  rgba[4]; /// array of the 4 color component
+#if IS_LITTLE_ENDIAN
+		struct
+		{
+			uint8_t a;
+			uint8_t b;
+			uint8_t g;
+		 	uint8_t r;
+		};
+	};
+#else
+		struct
+		{
+			uint8_t r;
+			uint8_t g;
+			uint8_t b;
+			uint8_t a;
+		};
+	};
+#endif
 }
 
 #endif // !COLOR_HPP_
