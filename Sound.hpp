83bf66fa8c02a144f144e4c2650ca29d4d9e06c9
Ludovic Petrenko <ludovic.petrenko@epitech.eu>
2017-03-30 06:03:47 +0200

    Added doc and STOP to SoundMode enum

--- Sound.hpp
+++ Sound.hpp
@@ -7,34 +7,52 @@
 
 namespace arcade
 {
-    //TODO : DOC
 
+    ///
+    /// \enum SoundMode
+    /// \brief Used to control the way sound is played
+    ///
     enum SoundMode
     {
-        UNIQUE,
-        REPEAT,
-        PAUSE,
-        RESUME
-    };
-
-    enum SoundType
-    {
-        MUSIC,
-        SOUND
+        UNIQUE, /// Sound played once
+        REPEAT, /// Sound played indefinitly (common for musics)
+        PAUSE, /// Pause a sound
+        RESUME, /// Resume a sound
+        STOP /// Stop completly a sound
     };
 
     ///
-    /// \fn SoundMode
-    /// \brief Contain the id of the sound and the mode (unique or infinite)
+    /// \enum SoundType
+    /// \brief Used to differenciate the sound type
+    ///
+    /// It might be very usefull with some sound libraries which need to know
+    /// this type of differences
+    ///
+    enum SoundType
+    {
+        MUSIC, /// The sound is a music
+        SOUND /// The sound is an normal sound
+    };
+
+    ///
+    /// \struct SoundMode
+    /// \brief Contain informations on the sound to play and the way of playing it
     ///
     struct Sound
     {
-        unsigned int	id;
-        SoundMode		mode;
-        SoundType       type;
-        float           volume;
-
+        ///
+        /// \fn Sound(unsigned int id, SoundMode mode = UNIQUE, SoundType type = SOUND, float volume = 50.0f);
+        /// \brief Constructor of a sound
+        /// \param id Id du son à controller
+        /// \param mode Mode du son
+        /// \param type Type de son
+        /// \param volume Volume du son
         Sound(unsigned int id, SoundMode mode = UNIQUE, SoundType type = SOUND, float volume = 50.0f);
+        
+        unsigned int	id; /// Id of the sound
+        SoundMode		mode; /// Mode of the sound (control)
+        SoundType       type; /// Type of the sound
+        float           volume; /// Volume
     };
 
 }
